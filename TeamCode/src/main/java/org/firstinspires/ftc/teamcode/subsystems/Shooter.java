545b44554546e999c08af3e0ecf57eb90eaa6bab
levistewart1123 <levistewart1123@gmail.com>
2026-05-14 17:57:21 -0400

    worked a bit on shooter, added LUTs and unused state machine, deleted solverslib command samples

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -1,21 +1,40 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.hardware.motors.Motor;
 import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
 import com.seattlesolvers.solverslib.hardware.motors.MotorGroup;
 import com.seattlesolvers.solverslib.hardware.servos.ServoEx;
+import com.seattlesolvers.solverslib.util.InterpLUT;
+
+import java.util.Objects;
 
 public class Shooter {
+
+    public enum states{
+        AUTOMATIC,
+        FIXED,
+        OFF
+    }
+
+    states state = states.AUTOMATIC;
+
     private MotorGroup flywheels;
     private ServoEx gate;
     private ServoEx hood;
+    final double FIXED_SPEED = 0.5;
+
+    public double targetRPM = 0;
+
+    InterpLUT velocities = new InterpLUT();
+    InterpLUT angles = new InterpLUT();
+
+    boolean launching = false;
 
     public void init(HardwareMap hwMap){
         flywheels = new MotorGroup(
-                new MotorEx(hwMap, "FlywheelLeft", Motor.GoBILDA.NONE).setInverted(true),
-                new MotorEx(hwMap, "FlywheelRight", Motor.GoBILDA.NONE)
+                new MotorEx(hwMap, "FlywheelLeft", Motor.GoBILDA.NONE.getCPR(), 4825).setInverted(true),
+                new MotorEx(hwMap, "FlywheelRight", Motor.GoBILDA.NONE.getCPR(), 4825)
         );
         flywheels.setRunMode(Motor.RunMode.VelocityControl);
 
@@ -27,15 +46,36 @@ public class Shooter {
 
         hood = hwMap.get(ServoEx.class, "Hood");
         hood.setInverted(true);
-    }
 
-    public void setRPM(double targetRPM){
-        flywheels.set(targetRPM);
-    }
+        velocities.add(1, 1); //TODO: replace this
+        velocities.createLUT();
+        angles.add(1, 1); //TODO: replace this
+        angles.createLUT();
 
-    public void target(double distance){
-
+        state = states.AUTOMATIC;
     }
+//?uncertain if this will be used, commenting it out now
+
+//    public void periodic(double distance){
+//        if (state == states.AUTOMATIC) {
+//            targetRPM = velocities.get(distance);
+//            flywheels.set(targetRPM / flywheels.getMaxRPM());
+//            hood.set(angles.get(distance));
+//        }
+//    }
+//    public void changeState(states state){
+//        this.state = state;
+//        switch(state){
+//            case OFF:
+//                flywheels.set(0);
+//                break;
+//            case FIXED:
+//                flywheels.set(FIXED_SPEED);
+//                hood.set(0);
+//                break;
+//        }
+//
+//    }
 
 
 }
