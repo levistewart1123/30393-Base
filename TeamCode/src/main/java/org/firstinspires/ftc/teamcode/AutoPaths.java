c6fb8f089459258d3a33ad39c9443fdb343a0dec
TechBoi387 <kevinbergel772@gmail.com>
2026-06-04 18:00:01 -0400

    Added Poses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPaths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPaths.java
@@ -6,21 +6,30 @@ import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 
 public class AutoPaths {
-
-    public Pose start, shoot, farRightCollect, farLeftCollect, gateCollect, spikeMarkI, spikeMarkII, spikeMarkIII;
-    public PathChain startToShoot, shootToFarRightCollect, shootToFarLeftCollect, farLeftCollectToShoot, farRightCollectToShoot, shootToSpikeMarkI,shootToSpikeMarkII, shootToGateCollect, spikeMarkIToShoot, spikeMarkIIToShoot, gateCollectToShoot;
+    public Pose start, shoot, farHumanPlayerCollect, farHighHumanPlayerCollect, gateCollect, spikeMarkTop, spikeMarkMiddle, spikeMarkBottom;
+    public PathChain startToShoot, shootToFarRightCollect, shootToFarLeftCollect, farLeftCollectToShoot, farHumanPlayerCollectToShoot, shootToSpikeMarkTop,shootToSpikeMarkMiddle, shootToGateCollect, spikeMarkOneToShoot, spikeMarkMiddleToShoot, gateCollectToShoot;
 
     public AutoPaths(Follower follower, boolean isRed, boolean far) {
+        farHumanPlayerCollect = new Pose(10, 4, Math.toRadians(180)); //TODO Add pose here
+        farHighHumanPlayerCollect = new Pose(10,25.000, Math.toRadians(180)); //TODO Add pose here
+        gateCollect = new Pose(14.4, 58.2, Math.toRadians(144.9));
+        spikeMarkTop = new Pose(22.1, 82.7, Math.toRadians(180));
+        spikeMarkMiddle = new Pose(15.0, 56.5, Math.toRadians(180));
+        spikeMarkBottom = new Pose(11.6, 40, Math.toRadians(180));
+
         if (far) {// When far
-            start = new Pose(134, 139, Math.toRadians(180));//! may not work due to reversing start heading
-            shoot = new Pose(134, 139, Math.toRadians(216));
-            farRightCollect = new Pose(); //TODO Add pose here
-            farLeftCollect = new Pose(); //TODO Add pose here
+            start = new Pose(55.6, 7.2, Math.toRadians(90));//! may not work due to reversing start heading
+            shoot = new Pose(56.9, 20.6, Math.toRadians(118.5));
+
             if (isRed) {
                 start.mirror();
                 shoot.mirror();
-                farRightCollect.mirror();
-                farLeftCollect.mirror();
+                farHumanPlayerCollect.mirror();
+                farHighHumanPlayerCollect.mirror();
+                gateCollect.mirror();
+                spikeMarkTop.mirror();
+                spikeMarkMiddle.mirror();
+                spikeMarkBottom.mirror();
             }
             startToShoot = follower.pathBuilder()
                     .addPath(new BezierLine(
@@ -32,43 +41,41 @@ public class AutoPaths {
             shootToFarRightCollect = follower.pathBuilder()
                     .addPath(new BezierLine(
                             shoot,
-                            farRightCollect
+                            farHumanPlayerCollect
                     ))
-                    .setLinearHeadingInterpolation(shoot.getHeading(), farRightCollect.getHeading())
+                    .setLinearHeadingInterpolation(shoot.getHeading(), farHumanPlayerCollect.getHeading())
                     .build();
             shootToFarLeftCollect = follower.pathBuilder()
                     .addPath(new BezierLine(
                             shoot,
-                            farLeftCollect
+                            farHighHumanPlayerCollect
                     ))
-                    .setLinearHeadingInterpolation(shoot.getHeading(), farLeftCollect.getHeading())
+                    .setLinearHeadingInterpolation(shoot.getHeading(), farHighHumanPlayerCollect.getHeading())
                     .build();
             farLeftCollectToShoot = follower.pathBuilder()
                     .addPath(new BezierLine(
-                            farLeftCollect,
+                            farHighHumanPlayerCollect,
                             shoot
                     ))
-                    .setLinearHeadingInterpolation(farLeftCollect.getHeading(), shoot.getHeading())
+                    .setLinearHeadingInterpolation(farHighHumanPlayerCollect.getHeading(), shoot.getHeading())
                     .build();
-            farRightCollectToShoot = follower.pathBuilder()
+            farHumanPlayerCollectToShoot = follower.pathBuilder()
                     .addPath(new BezierLine(
-                            farRightCollect,
+                            farHumanPlayerCollect,
                             shoot
                     ))
-                    .setLinearHeadingInterpolation(farRightCollect.getHeading(), shoot.getHeading())
+                    .setLinearHeadingInterpolation(farHumanPlayerCollect.getHeading(), shoot.getHeading())
                     .build();
         } else if (!far) {// When close
-            start = new Pose();//TODO Add poses here
-            shoot = new Pose();//TODO Add poses here
-            gateCollect = new Pose();//TODO Add poses here
-            spikeMarkI = new Pose();//TODO Add poses here
-            spikeMarkII = new Pose();//TODO Add poses here
+            start = new Pose(22.3, 120.2, Math.toRadians(139.4));//TODO Add poses here
+            shoot = new Pose(59.6, 72.9,Math.toRadians(130));//TODO Add poses here
+
             if (isRed){
                 start.mirror();
                 shoot.mirror();
                 gateCollect.mirror();
-                spikeMarkI.mirror();
-                spikeMarkII.mirror();
+                spikeMarkTop.mirror();
+                spikeMarkMiddle.mirror();
             }
             startToShoot = follower.pathBuilder()
                     .addPath(new BezierLine(
@@ -77,19 +84,19 @@ public class AutoPaths {
                     ))
                     .setLinearHeadingInterpolation(start.getHeading(), shoot.getHeading())
                     .build();
-            shootToSpikeMarkI = follower.pathBuilder()
+            shootToSpikeMarkTop = follower.pathBuilder()
                     .addPath(new BezierLine(
                             shoot,
-                            spikeMarkI
+                            spikeMarkTop
                     ))
-                    .setLinearHeadingInterpolation(shoot.getHeading(), spikeMarkI.getHeading())
+                    .setLinearHeadingInterpolation(shoot.getHeading(), spikeMarkTop.getHeading())
                     .build();
-            shootToSpikeMarkII = follower.pathBuilder()
+            shootToSpikeMarkMiddle = follower.pathBuilder()
                     .addPath(new BezierLine(
                             shoot,
-                            spikeMarkII
+                            spikeMarkMiddle
                     ))
-                    .setLinearHeadingInterpolation(shoot.getHeading(), spikeMarkII.getHeading())
+                    .setLinearHeadingInterpolation(shoot.getHeading(), spikeMarkMiddle.getHeading())
                     .build();
             shootToGateCollect = follower.pathBuilder()
                     .addPath(new BezierLine(
@@ -105,19 +112,19 @@ public class AutoPaths {
                     ))
                     .setLinearHeadingInterpolation(gateCollect.getHeading(), shoot.getHeading())
                     .build();
-            spikeMarkIToShoot = follower.pathBuilder()
+            spikeMarkOneToShoot = follower.pathBuilder()
                     .addPath(new BezierLine(
-                            spikeMarkI,
+                            spikeMarkTop,
                             shoot
                     ))
-                    .setLinearHeadingInterpolation(spikeMarkI.getHeading(), shoot.getHeading())
+                    .setLinearHeadingInterpolation(spikeMarkTop.getHeading(), shoot.getHeading())
                     .build();
-            spikeMarkIIToShoot = follower.pathBuilder()
+            spikeMarkMiddleToShoot = follower.pathBuilder()
                     .addPath(new BezierLine(
-                            spikeMarkII,
+                            spikeMarkMiddle,
                             shoot
                     ))
-                    .setLinearHeadingInterpolation(spikeMarkII.getHeading(), shoot.getHeading())
+                    .setLinearHeadingInterpolation(spikeMarkMiddle.getHeading(), shoot.getHeading())
                     .build();
         }
         //etc
