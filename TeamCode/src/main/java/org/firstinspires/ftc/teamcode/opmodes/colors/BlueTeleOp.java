b5bca3b4fea514837464904ed5098ef9be794c41
levistewart1123 <levistewart1123@gmail.com>
2026-06-22 14:34:27 -0400

    Set up after HRI

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/colors/BlueTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/colors/BlueTeleOp.java
@@ -1,6 +1,9 @@
-package org.firstinspires.ftc.teamcode.opmodes.tele;
+package org.firstinspires.ftc.teamcode.opmodes.colors;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.opmodes.tele.BaseTeleOp;
+
 
 @TeleOp(name = "Blue TeleOp", group = "0: TeleOp")
 public class BlueTeleOp extends BaseTeleOp {
